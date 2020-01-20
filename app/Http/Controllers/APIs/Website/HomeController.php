72eae1f1db4b0cca25caf25a6fd8db15a54d8684
hosam ameen <hosamameen948@gmail.com>
2020-01-20 14:02:12 +0200

    edit services

--- app/Http/Controllers/APIs/Website/HomeController.php
+++ app/Http/Controllers/APIs/Website/HomeController.php
@@ -38,8 +38,7 @@ class HomeController extends Controller
 
     public function showServices(Request $request)
     {
-
-        return $this->APIResponse( \App\Models\ServiceCategory::where('category_id' , $request->category_id)->get(['id','name']), null, 200);
+        return $this->APIResponse( \App\Models\ServiceCategory::where('category_id' , $request->category_id)->get(), null, 200);
     }
     public function showCountries()
     {
