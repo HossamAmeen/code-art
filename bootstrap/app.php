f1e0943753dfbf85c2e20600a917341f037630f8
hosam ameen <hosamameen948@gmail.com>
2020-02-06 12:10:04 +0200

    edit cors with array header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@
 |
 */
 header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
+// header('Access-Control-Allow-Methods', "'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'");
+header('Access-Control-Allow-Methods', "GET,POST,PUT,PATCH,DELETE,OPTIONS");
 header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 
